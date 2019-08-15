4c5a534348d272a648cef2ddf5b58d89cf1d76d2
Aspy <52931655+AspySs@users.noreply.github.com>
2019-08-15 19:01:30 +0300

    reg

--- regist.php
+++ regist.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 if(isset($_POST["submit"])){
 $pass = strip_tags($_POST["pass"]);
-$name = strip_tags($_POST["name"]);
+$mail = strip_tags($_POST["mail"]);
 $pass1 = strip_tags($_POST["pass1"]);
 $salt = "d9DqJluFQH3N2jE8LQvd";
 
@@ -9,13 +9,13 @@ $salt = "d9DqJluFQH3N2jE8LQvd";
  if($pass == $pass1){
 $passmd5 = md5($pass.$salt);
 require "bd.php";
-$result = $bd -> query("INSERT INTO `users` (`id`, `name`, `pass`) VALUES (NULL, '".$name."', '".$passmd5."');");
+$result = $bd -> query("INSERT INTO `users` (`id`, `mail`, `pass`) VALUES (NULL, '".$mail."', '".$passmd5."');");
 $bd -> close();
 if ($result == 1){
 	session_set_cookie_params(60);
 	session_start();
-	$errorname = "";
-	$_SESSION["regerrorNAME"] = $errorname;
+	$errormail = "";
+	$_SESSION["regerrormail"] = $errormail;
 	$_SESSION["auth"] = true;
 	header('Location: index.html');
 
@@ -25,8 +25,8 @@ if ($result == 1){
 }else{
 	session_set_cookie_params(60);
 	session_start();
-	$errorname = "данное имя уже занято";
-	$_SESSION["regerrorNAME"] = $errorname;
+	$errormail = "данное имя уже занято";
+	$_SESSION["regerrormail"] = $errormail;
 	$_SESSION["auth"] = false;
 	header('Location: reg.php');
 }
@@ -57,13 +57,31 @@ if ($result == 1){
 	session_start();
 	$_SESSION["auth"] = false;
 	header('Location: reg.php');
+}else{header('Location: reg.php');}
+
+
+
+
+
+function pochta(){
+
+$to  = "<mail@example.com>"; 
+
+$subject = "Регистрация на сервисе DOGE CASH"; 
+
+$message = ' <p>Здравствуйте, '.$name.' !</p> </br>Ваш логин: '.$mail.'  </br></br>';
+
+$headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
+$headers .= "From: От кого письмо <from@example.com>\r\n"; 
+$headers .= "Reply-To: reply-to@example.com\r\n"; 
+
+mail($to, $subject, $message, $headers);
+
+
+
+
+
 }
-
-
-
-
-
-
 
 
 
