81c5aac84fbc3a04e10e076c51766886505ac902
Aspy <52931655+AspySs@users.noreply.github.com>
2019-08-15 23:25:44 +0300

    add mail message and link

--- regist.php
+++ regist.php
@@ -4,18 +4,20 @@ $pass = strip_tags($_POST["pass"]);
 $mail = strip_tags($_POST["mail"]);
 $name = strip_tags($_POST["name"]);
 $pass1 = strip_tags($_POST["pass1"]);
+$codemail = generateSTR();
+$mailstatus = "false";
 $salt = "d9DqJluFQH3N2jE8LQvd";
 
 	if(($pass != " ")&&($pass != "")&&($pass != NULL)){
  if($pass == $pass1){
 $passmd5 = md5($pass.$salt);
 require "bd.php";
-$result = $bd -> query("INSERT INTO `users` (`id`, `mail`, `name`, `pass`) VALUES (NULL, '".$mail."', '".$name."', '".$passmd5."');");
+$result = $bd -> query("INSERT INTO `users` (`id`, `mail`, `name`, `pass`, `mailcode`, `mailstatus`) VALUES (NULL, '".$mail."', '".$name."', '".$passmd5."', '".$codemail."', '".$mailstatus."');");
 $bd -> close();
 if ($result == 1){
 	session_set_cookie_params(60);
 	session_start();
-	pochta($mail, $name, $pass);
+	pochta($mail, $name, $pass, $codemail);
 	$errormail = "";
 	$_SESSION["regerrormail"] = $errormail;
 	$_SESSION["auth"] = true;
@@ -65,16 +67,16 @@ if ($result == 1){
 
 
 
-function pochta($mail, $name, $pass){
+function pochta($mail, $name, $pass, $codemail){
 
 $to  = "<".$mail.">"; 
 
 $subject = "Регистрация на сервисе DOGE CASH"; 
 
-$message = ' <p>Здравствуйте, '.$name.' !</p> </br>Ваш логин: '.$mail.'  </br> Ваш пароль: '.$pass.'</br> </br> Ваш BTC счет: 17ghA6SYe4x3joxqhkDTAJy14ezPENhmBB</br></br>Это письмо отправлено роботом, отвечать на него не нужно! </br>';
+$message = "<p>Здравствуйте, ".$name." !</p> </br>Ваш логин: ".$mail."  </br> Ваш пароль: ".$pass."</br> </br> Ваш BTC счет: 17ghA6SYe4x3joxqhkDTAJy14ezPENhmBB</br></br>Это письмо отправлено роботом, отвечать на него не нужно! </br> </br>Нажмите на кнопку ниже чтобы подтвердить свою почту </br> </br><a style = \"background: #6D43FF padding: 5px border-radius: 12px font-weight: bold color: #fff\" href=\"http://cash.sphinxs.ru/mail.php?code=".$codemail."\">Подтвердить почту</a></br>";
 
-$headers  = "Content-type: text/html; charset=windows-1251 \r\n"; 
-$headers .= "From: От кого письмо <no.reply.bot@sphinxs.ru>\r\n"; 
+$headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
+$headers .= "From: SPHINXS-BOT <no.reply.bot@sphinxs.ru>\r\n"; 
 $headers .= "Reply-To: no.reply.bot@sphinxs.ru\r\n"; 
 
 mail($to, $subject, $message, $headers);
@@ -85,5 +87,16 @@ mail($to, $subject, $message, $headers);
 
 }
 
+
+
+function generateSTR($length = 10){
+$chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+$numChars = strlen($chars);
+$string = '';
+for ($i = 0; $i < $length; $i++) {
+$string .= substr($chars, rand(1, $numChars) - 1, 1);
+}
+return $string;
+}
 
 ?>
