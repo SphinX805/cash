c316a5fadb45e1916add0c1c723ff401650f0487
SphinX805 <52954048+SphinX805@users.noreply.github.com>
2019-08-15 19:04:04 +0300

    style

--- reg.php
+++ reg.php
@@ -27,6 +27,7 @@ header('Location: index.php');
 	
 	<form method="POST" action="regist.php">
 		<div class="logo"><i class="fas fa-wallet"></i>DogeCash</div>
+		<p>Имя:</p><input type="text" placeholder="Придумайте имя" name="name"><br>
 		<p>Логин:</p><input type="text" placeholder="cash@gmail.com" name="name"> <p><font color="red"><?php echo @$_SESSION["regerrorNAME"]; ?></font></p><br>
 		<p>Пароль:</p><input type="password" name="pass"><br>
 		<p>Повтор пароля:</p><input type="password" name="pass1"><p><font color = "red"><?php echo @$_SESSION["regerrorPASS"]; ?></font><br>
