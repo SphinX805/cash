bf484a84b6bb73de7128484eb7df6133f268febf
Aspy <52931655+AspySs@users.noreply.github.com>
2019-08-16 12:45:08 +0300

    add style to other pages

--- account/promej/error.php
+++ account/promej/error.php
@@ -10,13 +10,59 @@ $id = $_SESSION["id"];
 <p><?php echo $message ?> </p>
 <a class="btn" href="../../index.php">link</a>
 
+<div class="message">	
+<p><?php echo $message ?></p>
+<a class="btn" href="../index.php"><p>link</p></a>
+</div>
+
 <style type="text/css">
-	.btn {
-		text-decoration: none;
+	body {
+		margin: 0;	
+		background-color: #0c0c0c;
+		background-image: url("https://www.transparenttextures.com/patterns/cubes.png");
 		color: #fff;
-		background-color: #000;
+	}
+
+	.message {
+		width: 40%;
+		min-height: 80px;
+		height: auto;
+		margin: 40px 30%;
+		border-radius: 	5px;
+		background-color: rgba(45,45,45,0.85);
+	}
+
+	.message p {
+		display: inline-block;
+		width: 50%;	
+	}
+
+	.btn {
+		float: right;		
+		width: 80px;
+		height: 25px;
+		padding: 5px 0 5px 20px;	
+		margin: 10px 30px;
+		color: #fff;
+		background-color: #7C1177;
+		text-decoration: none;
 		border-radius: 8px;
-		padding: 5px 10px;
+	}
+
+	.btn p {
+		display: inline-block;
+		width: 60px;	
+		text-align: center;
+		padding: 0;	
+		margin: 0;
+		color: #FFAAFF;	
+		font-weight: bold;
+		font-size: 	20px;
+		border-bottom: 2px solid #7C1177;
+	}
+		
+	.btn:hover p {
+		border-bottom: 2px solid #fff;
 	}
 
 	p {
@@ -24,3 +70,4 @@ $id = $_SESSION["id"];
 		margin: 20px 40px;
 	}
 </style>
+
