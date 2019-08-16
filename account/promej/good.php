f921c4b4181168fc6edb097a74e2be93585b39f7
SphinX805 <52954048+SphinX805@users.noreply.github.com>
2019-08-16 09:49:04 +0300

    Update good.php

--- account/promej/good.php
+++ account/promej/good.php
@@ -1,11 +1,26 @@
 <?php
-session_set_cookie_params(21600);
-session_start();
-$status = $_SESSION["maiSTAT"];
-$message = $_SESSION["message"];
+// session_set_cookie_params(21600);
+// session_start();
+// $status = $_SESSION["maiSTAT"];
+// $message = $_SESSION["message"];
 ?>
 
 
+<p>message</p>
+<a class="btn" href="#">link</a>
+<!-- <p> <?php echo $message;  ?> </p> -->
 
+<style type="text/css">
+	.btn {
+		text-decoration: none;
+		color: #fff;
+		background-color: #000;
+		border-radius: 8px;
+		padding: 5px 10px;
+	}
 
-<p> <?php echo $message;  ?> </p>
+	p {
+		display: inline-block;
+		margin: 20px 40px;
+	}
+</style>
